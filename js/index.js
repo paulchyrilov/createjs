19b37794d9a6155a5e4f857afe5152652ea7ef2b
paul <paulchyrilov@gmail.com>
2016-02-05 02:04:39 +0300

    simple walking man

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ function init() {
 
     manifest = [
         {src: "wave.png", id: "wave"},
-        {src: "waves.png", id: "wave.sprite"}
+        {src: "sprite.png", id: "wave.sprite"}
     ];
 
     loader = new createjs.LoadQueue(false);
@@ -31,43 +31,105 @@ function loadComplete() {
 
     spriteSheet = new createjs.SpriteSheet({
         images: [ loader.getResult("wave.sprite") ],
-        frames: { width: 200, height: 200, count: 5 },
+        frames: { width: 64, height: 64},
         animations: {
-            idle: [ 0, 4, 'idle', 0.1 ]
+            idle: [ 0, 2, 'idle', 0.05 ],
+            run: {frames: [3,4,5,4],
+                next: "run",
+                speed: 0.08
+            },
+            stop: [ 5, 5, 'idle', 1 ],
+            jump: [ 6, 7, 'idle', 0.08 ],
+            jumpAndRun: [ 6, 7, 'run', 0.08 ]
         }
     });
 
     animation = new createjs.Sprite(spriteSheet);
     animation.gotoAndPlay('idle');
+    animation.speed = 3;
+
+    animation.moveleft = function(){
+        if(this.moving) {
+            return;
+        }
+        this.moving = true;
+        if (this.scaleX > 0) {
+            this.scaleX *= -1;
+            this.x += this.getBounds().width;
+        }
+        this.gotoAndPlay('run');
+    };
+    animation.moveright = function(){
+        if(this.moving) {
+            return;
+        }
+        this.moving = true;
+        if (this.scaleX < 0) {
+            this.scaleX *= -1;
+            this.x -= this.getBounds().width;
+        }
+        this.gotoAndPlay('run');
+    };
+    animation.stop = function(){
+        if(!this.moving) {
+            return;
+        }
+        this.moving = false;
+        this.gotoAndPlay('stop');
+    };
+    animation.jump = function(){
+        if(this.jumping) {
+            return;
+        }
+        this.jumping = true;
+        if(this.moving) {
+            this.gotoAndPlay('jumpAndRun')
+        } else {
+            this.gotoAndPlay('jump');
+        }
+    };
+    animation.checkAndMove = function(dx) {
+        if(!this.moving) {
+            return;
+        }
+        dx *= this.speed;
+        var direction = 1;
+        if (this.scaleX < 0) {
+            direction = -1;
+        }
+        var newPosition = this.x + direction * (this.getBounds().width + dx)
+        if(newPosition > w || newPosition < 0) {
+            return;
+        }
+        this.x += dx * direction;
+    };
 
     stage.addChild(animation);
 
-    var seaImg = loader.getResult("wave");
-    sea = new createjs.Shape();
-    sea.graphics.beginBitmapFill(seaImg).drawRect(0, 0, w + seaImg.width, seaImg.height);
-    sea.tileW = seaImg.width;
-    sea.y = h - 1.3*seaImg.height;
-    sea.initialX = sea.x = -seaImg.width;
-    sea.speed = 1;
-    stage.addChild(sea);
-
-    var dx = 50;
-
-    sea2 = new createjs.Shape();
-    sea2.graphics.beginBitmapFill(seaImg).drawRect(0, 0, w + seaImg.width + dx, seaImg.height);
-    sea2.tileW = seaImg.width;
-    sea2.y = h - seaImg.height;
-    sea2.x = -(seaImg.width + dx);
-    sea2.initialX = sea2.x;
-    sea2.speed = 0.7;
-    stage.addChild(sea2);
-
-    sea.move = sea2.move = function(dx){
-        if(this.x > this.initialX + this.tileW) {
-            dx += this.initialX + this.tileW - this.x;
-            this.x = this.initialX;
+    document.onkeydown = function(e){
+        switch (e.keyCode) {
+            case 87:  // W
+                animation.jump();
+                break;
+            case 65:  // A
+                animation.moveleft();
+                break;
+            case 68:  // D
+                animation.moveright();
+                break;
         }
-        this.x += dx * this.speed;
+    };
+
+    document.onkeyup = function(e){
+        switch (e.keyCode) {
+            case 65:  // A
+            case 68:  // D
+                animation.stop();
+                break;
+            case 87:  // W
+                animation.jumping = false;
+
+        }
     };
 
     if(typeof requestAnimationFrame == 'function') {
@@ -80,8 +142,11 @@ function tick(event) {
 
     var dx = event.delta / 50;
 
-    sea.move(dx);
-    sea2.move(dx);
+    animation.checkAndMove(dx);
+
+    animation.on('click', function(){
+       this.gotoAndPlay('jump');
+    });
 
     stage.update(event);
 }
